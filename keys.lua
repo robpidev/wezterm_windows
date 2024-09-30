1756ee6f7cc6ebbc145faaf1cc0db219da8955b1
Rober <robertdrakenfor@gmail.com>
2024-09-30 10:14:07 -0500

    config

--- keys.lua
+++ keys.lua
@@ -5,35 +5,35 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 
 for i = 1, 9 do
-	table.insert(keys, {
-		key = tostring(i),
-		mods = "CTRL",
-		action = act.ActivateTab(i - 1),
-	})
+  table.insert(keys, {
+    key = tostring(i),
+    mods = "CTRL",
+    action = act.ActivateTab(i - 1),
+  })
 end
 
 table.insert(keys, {
-	key = "t",
-	mods = "CTRL",
-	action = act.SpawnTab("CurrentPaneDomain"),
+  key = "t",
+  mods = "CTRL",
+  action = act.SpawnTab("CurrentPaneDomain"),
 })
 
 table.insert(keys, {
-	key = "w",
-	mods = "CTRL",
-	action = act.CloseCurrentTab({ confirm = true }),
+  key = "w",
+  mods = "CTRL",
+  action = act.CloseCurrentTab({ confirm = true }),
 })
 --
-table.insert(keys, {
-	key = "c",
-	mods = "CTRL",
-	action = act.CopyTo("Clipboard"),
-})
+-- table.insert(keys, {
+-- 	key = "c",
+-- 	mods = "CTRL",
+-- 	action = act.CopyTo("Clipboard"),
+-- })
 
 table.insert(keys, {
-	key = "v",
-	mods = "CTRL",
-	action = act.PasteFrom("Clipboard"),
+  key = "v",
+  mods = "CTRL",
+  action = act.PasteFrom("Clipboard"),
 })
 
 return keys
